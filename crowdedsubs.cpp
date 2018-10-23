892e5d654666b92724c761f0b3ae0524833bb12b
T1duS <udit121sanghi@gmail.com>
2018-10-24 01:07:43 +0530

    Added Commits and did some improvements

--- crowdedsubs.cpp
+++ crowdedsubs.cpp
@@ -1,18 +1,27 @@
-#include<bits/stdc++.h>
-#include "srtparser.h"
+#include<bits/stdc++.h> // includes all the standard c++ libraries
+#include "srtparser.h" // includes srtparser made by Saurabh Shrivastava
 
 using namespace std;
 
+// made another struct to store subtitle info cuz I don't like dealing with pointers and stuff
 struct subinfo{
   int start,finish,wordcount;
   string thesub;
 };
 
 int main(int argc, char** argv){
-  SubtitleParserFactory *subParserFactory = new SubtitleParserFactory(argv[1]);
-  SubtitleParser *parser = subParserFactory->getParser();
+
+  if(argc < 4){
+    cout << "Too few arguments\n";
+    return 0;
+  }
+
+  SubtitleParserFactory *subParserFactory = new SubtitleParserFactory(argv[1]); // Initializing SrtParser Factory object
+  SubtitleParser *parser = subParserFactory->getParser(); // Snippet picked from srtparser readme file, idk what it means as well
+
   vector<SubtitleItem*> sub = parser->getSubtitles();
   vector<subinfo> v;
+  // Adding all the required subtitle info to a vector
   for(SubtitleItem * element : sub){
         subinfo current;
         current.start = element->getStartTime();
@@ -21,8 +30,10 @@ int main(int argc, char** argv){
         current.wordcount = element->getWordCount();
         v.push_back(current);
   }
-  int range = atoi(argv[2])*1000;
+  int range = atoi(argv[2])*1000; // range in milli seconds
   string thetype = argv[3];
+
+  // Using 2-pointer technique to calculate the answer
   int startindex,endindex,index = 0,currentmax = 0,currentscore = 0;
   for(int i = 0; i < v.size(); i ++){
     while(index < v.size()){
@@ -41,5 +52,20 @@ int main(int argc, char** argv){
     if(thetype == "sub") currentscore --;
     else if(thetype == "word")currentscore -= v[i].wordcount;
   }
-  cout << currentmax << endl;
+  // ------------------------------------------------
+
+  // Now is just the printing stuff
+
+  cout << "The " << atoi(argv[2]) << " seconds range with most amount of " << thetype << "s has " << currentmax << " " << thetype << "s." << endl;
+
+  cout << "Type 1 if you want to see the list of subs: " << flush;
+
+  char inputforsubshowing; cin >> inputforsubshowing;
+  if(inputforsubshowing != '1') return 0;
+
+
+
+  for(int i = startindex; i != endindex; i ++){
+    cout << v[i].start << " - " << v[i].finish << " : " << v[i].thesub << endl;
+  }
 }
